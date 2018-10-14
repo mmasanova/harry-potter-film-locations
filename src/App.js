2a6506c324e31c081f27993ad56c97e2e534bce9
Martina Masanova <martina.masanova@gmail.com>
2018-10-14 10:37:55 +0100

    feat: set map bounds

--- src/App.js
+++ src/App.js
@@ -7,32 +7,42 @@ class App extends Component {
     locations: [
       {
         name: 'The Falcon',
-        lat: 52.122060,
-        lng: -1.403040,
+        position: {
+          lat: 52.122060,
+          lng: -1.403040
+        },
         address: 'Warwick Road Warmington, Banbury OX17 1JJ'
       },
       {
         name: 'The Chequers',
-        lat: 51.942820,
-        lng: -1.544950,
+        position: {
+          lat: 51.942820,
+          lng: -1.544950
+        },
         address: 'Goddards Ln, Chipping Norton OX7 5NP'
       },
       {
         name: 'The Fox',
-        lat: 51.939730,
-        lng: -1.542790,
+        position: {
+          lat: 51.939730,
+          lng: -1.542790
+        },
         address: 'Chipping Norton OX7 5DD'
       },
       {
         name: 'The Three Pigeon\'s Inn',
-        lat: 52.063600,
-        lng: -1.339780,
+        position: {
+          lat: 52.063600,
+          lng: -1.339780
+        },
         address: '3 Southam Rd, Banbury OX16 2ED'
       },
       {
         name: 'The White Horse',
-        lat: 52.021520,
-        lng: -1.275970,
+        position: {
+          lat: 52.021520,
+          lng: -1.275970
+        },
         address: '2 The Square, King\'s Sutton, Banbury OX17 3RF'
       }
     ]
--- src/components/map-container.js
+++ src/components/map-container.js
@@ -2,22 +2,37 @@ import React, { Component } from 'react';
 import { Map, Marker } from 'google-maps-react';
 
 export class MapContainer extends Component {
+	state = {
+		bounds: {}
+	}
+
+	onMapReady(mapProps, map) {
+		// Set map bounds in order to display all markers
+		let bounds = new window.google.maps.LatLngBounds();
+
+		mapProps.children.forEach(child => {
+			bounds.extend(child.props.position);
+		});
+
+		map.fitBounds(bounds);
+	}
+
 	render () {
-		const { locations } = this.props;
-		
+		const { locations, google } = this.props;
+
 		return (
 			<Map 
-				google={this.props.google} 
-				style={{ height: '100%', position: 'relative', width: '100%' }} 
-				zoom={14}
-				initialCenter={{ lat: 52.060020, lng: -1.340450 }}>
-			{ locations.map(location => (
-				<Marker 
-					position={{ lat: location.lat, lng: location.lng }}
-					name={location.name}
-					title={location.name}
-					key={location.name}
-					visible={true} />
+				google={google}
+				initialCenter={{ lat: 52.060020, lng: -1.340450 }}
+				onReady={this.onMapReady}
+				bounds={this.state.bounds}>
+				{ locations.map(location => (
+					<Marker 
+						position={location.position}
+						name={location.name}
+						title={location.name}
+						key={location.name}
+						visible={true} />
 				))}
 			</Map>
 		)
